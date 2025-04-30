1a65e8281dafc3690751a54c76b7f4598c08e0ce
Arthur Huan <Arthur-Huan@users.noreply.github.com>
2025-04-30 11:42:15 +0800

    Delete anova_tab

--- calculator_tabs/anova_tab.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QWidget
-
-from calculator_tabs.abstract_calculator_tab import AbstractCalculatorTab
-
-
-class ANOVATab(AbstractCalculatorTab):
-    def __init__(self, data_manager, parent=None):
-        super().__init__(data_manager, parent)
-        
